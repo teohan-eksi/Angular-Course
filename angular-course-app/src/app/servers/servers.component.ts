b0599a79921197bac753fa487e81a2bb0180b33a
Teohan Eksi <teohan.eksi@gmail.com>
2021-10-23 22:22:12 +0300

    finished components

--- angular-course-app/src/app/servers/servers.component.ts
+++ angular-course-app/src/app/servers/servers.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  //selector: '[app-servers]', select by attribute
+  //selector: '.app-servers', select by class
   selector: 'app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
