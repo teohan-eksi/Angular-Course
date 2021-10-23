fd2a0f3c21cc3383be97af46d53ed4696391da41
Teohan Eksi <teohan.eksi@gmail.com>
2021-10-23 21:22:15 +0300

    added ServerComponent to declerations

--- angular-course-app/src/app/app.module.ts
+++ angular-course-app/src/app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    //declare new components here
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule
