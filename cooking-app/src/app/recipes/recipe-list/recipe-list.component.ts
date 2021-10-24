1d344b861c7b016129ccbcbc9e35f5f31e819eb6
Teohan Eksi <teohan.eksi@gmail.com>
2021-10-24 21:45:21 +0300

    displayed recipes

--- cooking-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ cooking-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,8 +10,9 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent{
     recipes: Recipe[] = [
         new Recipe("Test recipe", "description of the test recipe",
+        "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.Re8FcdH8gx_bTC2RcVfFCAHaHa%26pid%3DApi&f=1"),
+        new Recipe("Test recipe", "description of the test recipe",
         "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.Re8FcdH8gx_bTC2RcVfFCAHaHa%26pid%3DApi&f=1")
-
     ];
 
 }
