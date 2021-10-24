2ff91eb372319c08d83dc867d79e82eb29a84874
Teohan Eksi <teohan.eksi@gmail.com>
2021-10-24 16:42:04 +0300

    added RecipeModel and recipe item html

--- cooking-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ cooking-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,6 @@
 import { Component } from "@angular/core";
 
-
+import { Recipe } from '../recipe.model';
 
 @Component({
     selector: 'app-recipe-list',
@@ -8,5 +8,10 @@ import { Component } from "@angular/core";
 })
 
 export class RecipeListComponent{
+    recipes: Recipe[] = [
+        new Recipe("Test recipe", "description of the test recipe",
+        "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.Re8FcdH8gx_bTC2RcVfFCAHaHa%26pid%3DApi&f=1")
+
+    ];
 
 }
