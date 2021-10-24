4b14a68ce4c3a11c0559cd62da4051a137bd2035
Teohan Eksi <teohan.eksi@gmail.com>
2021-10-24 14:05:53 +0300

    added components

---
+++ cooking-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+
+
+@Component({
+    selector: 'app-recipe-list',
+    templateUrl: 'recipe-list.component.html'
+})
+
+export class RecipeListComponent{
+
+}
