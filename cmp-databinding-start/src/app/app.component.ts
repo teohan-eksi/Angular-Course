8f8ce513b5de191ac1fc22e2261a16a636a3dea2
Teohan Eksi <teohan.eksi@gmail.com>
2021-10-25 16:27:12 +0300

    lifecycle hooks

--- cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/src/app/app.component.ts
@@ -1,14 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnChanges {
   serverElementsArray = [];
 
   getServerElements(serverElems){
     this.serverElementsArray.push(serverElems);
   }
+
+  constructor(){
+    console.log("app component constructor run");
+    
+  }
+
+  //use for reacting to changes
+  ngOnChanges(changes: SimpleChanges){
+    console.log("app component " + changes); 
+  }
+
+  ngOnInit(){
+    console.log("app component init"); 
+  }
+
+  ngDoCheck(){
+    console.log("app component DoCheck");
+  }
+
+  ngAfterContentInit(){
+    console.log("app component AfterContentInit");
+  }
+
+  //elements are in the DOM and rendered at this point.
+  ngAfterViewInit(){
+    console.log("app component AfterViewInit");
+  }
+
+  ngOnDestroy(){
+    console.log("app component onDestroy");
+  }
+
 }
