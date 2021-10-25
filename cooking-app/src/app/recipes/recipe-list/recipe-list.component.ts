045a249d81cce190ef7228e37aeafefa57484c92
Teohan Eksi <teohan.eksi@gmail.com>
2021-10-25 21:53:04 +0300

    passed recipe data with property binding

--- cooking-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ cooking-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,7 +11,7 @@ export class RecipeListComponent{
     recipes: Recipe[] = [
         new Recipe("Test recipe", "description of the test recipe",
         "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.Re8FcdH8gx_bTC2RcVfFCAHaHa%26pid%3DApi&f=1"),
-        new Recipe("Test recipe", "description of the test recipe",
+        new Recipe("Test recipe2", "description of the test recipe2",
         "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.Re8FcdH8gx_bTC2RcVfFCAHaHa%26pid%3DApi&f=1")
     ];
 
