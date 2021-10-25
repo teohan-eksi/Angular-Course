553bd8e61927d8d2a80773942c97ad2d1ae4fddb
Teohan Eksi <teohan.eksi@gmail.com>
2021-10-25 21:37:35 +0300

    added navigation with event binding and ngIf

--- cooking-app/src/app/app.component.ts
+++ cooking-app/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cooking-app';
+  loadedFeature: string = 'recipes'
+
+  onNavigate(feature){
+    this.loadedFeature = feature;
+  }
 }
--- cooking-app/src/app/header/header.component.ts
+++ cooking-app/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 
 
@@ -8,5 +8,9 @@ import { Component } from "@angular/core";
 })
 
 export class HeaderComponent{
+    @Output() featureSelected = new EventEmitter<string>();
 
+    onSelect(feature: string){
+        this.featureSelected.emit(feature);
+    }
 }
