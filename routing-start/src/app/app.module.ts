f7e76a5602a8466fd16fd769f43686601ee4c86b
Teohan Eksi <teohan.eksi@gmail.com>
2021-10-29 23:36:59 +0300

    crouting with parameters

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { RouterModule, Routes } from '@angular/router';
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'users', component: UsersComponent},
+  {path: 'users/:id', component: UsersComponent},
   {path: 'servers', component: ServersComponent}
 ];
 
